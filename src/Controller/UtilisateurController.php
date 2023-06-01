38699f6900ce8e34371c5644f5c3d95773d3371d
bouboucouscous <remi.coussypro@gmail.com>
2023-06-01 18:18:06 +0200

    avancé

--- src/Controller/UtilisateurController.php
+++ src/Controller/UtilisateurController.php
@@ -2,17 +2,61 @@
 
 namespace App\Controller;
 
+use App\Entity\Utilisateur;
+use App\Form\UtilisateurType;
+use App\Repository\UtilisateurRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
 
+#[Route('/utilisateur')]
 class UtilisateurController extends AbstractController
 {
-    #[Route(path:'/utilisateur', name: 'app_utilisateur')]
-    public function index(): Response
+    #[Route('/', name: 'app_utilisateur', methods: ['GET'])]
+    public function index(UserInterface $user): Response
     {
+        $userId = $user->getId();
         return $this->render('utilisateur/index.html.twig', [
             'controller_name' => 'UtilisateurController',
+            'id' => $userId
         ]);
+    }
+
+    #[Route('', name: 'app_utilisateur_show', methods: ['GET'])]
+    public function show(Utilisateur $utilisateur): Response
+    {
+        return $this->render('utilisateur/show.html.twig', [
+            'utilisateur' => $utilisateur,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_utilisateur_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Utilisateur $utilisateur, UtilisateurRepository $utilisateurRepository): Response
+    {
+        $form = $this->createForm(UtilisateurType::class, $utilisateur);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $utilisateurRepository->save($utilisateur, true);
+
+            return $this->redirectToRoute('app_utilisateur', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('utilisateur/edit.html.twig', [
+            'utilisateur' => $utilisateur,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_utilisateur_delete', methods: ['POST'])]
+    public function delete(Request $request, Utilisateur $utilisateur, UtilisateurRepository $utilisateurRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$utilisateur->getId(), $request->request->get('_token'))) {
+            $utilisateurRepository->remove($utilisateur, true);
+        }
+
+        return $this->redirectToRoute('app_utilisateur', [], Response::HTTP_SEE_OTHER);
     }
 }
